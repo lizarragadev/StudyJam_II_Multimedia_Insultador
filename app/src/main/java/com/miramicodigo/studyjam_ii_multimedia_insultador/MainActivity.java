0bed6edc7f716f1391daf1b0768fc59b55f32758
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2016-12-13 14:35:35 -0400

    Commit inicial

---
+++ app/src/main/java/com/miramicodigo/studyjam_ii_multimedia_insultador/MainActivity.java
@@ -0,0 +1,13 @@
+package com.miramicodigo.studyjam_ii_multimedia_insultador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
