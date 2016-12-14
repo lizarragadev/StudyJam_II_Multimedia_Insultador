939c2fc49803bb968d301a0b4fec1e8d5471b3b1
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2016-12-14 09:27:09 -0400

    Generacion de codigo fuente de toda la aplicacion

--- app/src/main/java/com/miramicodigo/studyjam_ii_multimedia_insultador/MainActivity.java
+++ app/src/main/java/com/miramicodigo/studyjam_ii_multimedia_insultador/MainActivity.java
@@ -1,14 +1,80 @@
 package com.miramicodigo.studyjam_ii_multimedia_insultador;
 
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SoundPool spIdiota, spCallate, spVeteAlDiablo;
+    private int resIdiota, resCallate, resVeteAlDiablo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        createSoundPool();
+    }
+
+    private void createSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            createNewSoundPool();
+        } else {
+            createOldSoundPool();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void createNewSoundPool(){
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        spIdiota = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        spCallate = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        spVeteAlDiablo = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+        chargeSoundPool();
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void createOldSoundPool(){
+        spIdiota = new SoundPool(15,AudioManager.STREAM_MUSIC,0);
+        spCallate = new SoundPool(10,AudioManager.STREAM_MUSIC,0);
+        spVeteAlDiablo = new SoundPool(12,AudioManager.STREAM_MUSIC,0);
+        chargeSoundPool();
+    }
+
+    public void chargeSoundPool() {
+        resIdiota = spIdiota.load(getApplicationContext(), R.raw.idiota, 1);
+        resCallate = spCallate.load(getApplicationContext(), R.raw.callate, 1);
+        resVeteAlDiablo = spVeteAlDiablo.load(getApplicationContext(), R.raw.vete_al_diablo, 1);
+    }
+
+    public void idiota(View v) {
+        if (resIdiota != 0){
+            spIdiota.play(resIdiota, 1, 1, 0, 0, 1);
+        }
+    }
+    public void callate(View v) {
+        if (resCallate != 0){
+            spCallate.play(resCallate, 1, 1, 0, 0, 1);
+        }
+    }
+    public void veteAlDiablo(View v) {
+        if (resVeteAlDiablo != 0){
+            spVeteAlDiablo.play(resVeteAlDiablo, 1, 1, 0, 0, 1);
+        }
     }
 }
